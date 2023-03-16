1a641f627bdc87c74ee27b3e3f2f91011f6f7bed
RICARDO DA COSTA PASCHOALI <47854361+HarryRicardo1989@users.noreply.github.com>
2023-03-16 00:08:32 -0300

    Update fuel_time2.c

--- fuel_time2.c
+++ fuel_time2.c
@@ -3,6 +3,13 @@
 #define bico 40
 #define cilinder_volume 224
 
+#define THERMISTOR_RESISTOR 4700.0 // valor do resistor em ohms
+#define THERMISTOR_A 0.000903621 // constante A do termistor
+#define THERMISTOR_B 0.000247207 // constante B do termistor
+#define THERMISTOR_C 9.47828e-08 // constante C do termistor
+#define THERMISTOR_REF_RESISTANCE 10000.0 // resistência de referência em ohms
+
+
 class Injector {
 public:
   double get_injection_time(double temp, double p_abs, double p_fuel) {
@@ -33,6 +40,13 @@ double read_pressure(double voltage) {
   // Retornar o valor em bar
   return pressure;
 }
-
+double get_temperature(double voltage) {
+  double resistance = THERMISTOR_RESISTOR * (5.0 / voltage - 1.0);
+  double ln_resistance = log(resistance);
+  double inv_temperature = THERMISTOR_A + THERMISTOR_B * ln_resistance 
+    + THERMISTOR_C * pow(ln_resistance, 3);
+  double temperature = 1.0 / inv_temperature - 273.15; // converter para Celsius
+  return temperature;
+}
 
 };
